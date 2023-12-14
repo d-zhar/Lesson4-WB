829b9a7e9e4224bb5511c8c2c40c827c489eba7d
d-zhar <d-zhar@list.ru>
2023-12-14 18:56:41 +0300

    first commit

---
+++ file.js
@@ -0,0 +1,19 @@
+// const values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// btn_plus.addEventListener("click", increment);
+// btn_minus.addEventListener("click", decrement);
+
+// function increment() {
+//   const nextIndex = getCurrentIndex() + 1;
+//   if (values[nextIndex]) inp.textContent = values[nextIndex];
+// }
+
+// function decrement() {
+//   const prevIndex = getCurrentIndex() - 1;
+//   if (values[prevIndex]) inp.textContent = values[prevIndex];
+// }
+
+// function getCurrentIndex() {
+//   const currentVal = parseInt(inp.textContent);
+//   return values.indexOf(currentVal);
+// }
